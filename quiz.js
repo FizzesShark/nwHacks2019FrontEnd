fdfbb9d91c56ad590e3e291754125aac98652137
FizzesShark <xbug2001@gmail.com>
2019-01-27 05:40:27 -0800

    Idk

--- quiz.js
+++ quiz.js
@@ -1,5 +1,6 @@
 chrome.identity.getProfileUserInfo(function(e){
   $.get("https://mykatz.lib.id/repeatedli@dev/quiz?id=" + e.id, function (f){
+    console.log(JSON.stringify(f));
     for (var i=1;i<6;i++){
       $("#basic-addon" + i).text(f["Q" + i].correct);
     };
@@ -15,7 +16,22 @@ chrome.identity.getProfileUserInfo(function(e){
           corrects[i - 1] = false;
         }
       };
-      $.get("https://mykatz.lib.id/repeatedli@dev/submitquiz?id=" + e.id + "&obj=" + JSON.stringify({"words": words, "corrects": corrects}));
+      $.get("https://mykatz.lib.id/repeatedli@dev/submitquiz?id=" + e.id + "&obj=" + JSON.stringify({"words": words, "corrects": corrects}), function(){
+        var amount = 0;
+        console.log(JSON.stringify({"words": words, "corrects": corrects}));
+        for (var i=1;i<6;i++){
+          if (corrects[i - 1]){
+            $("#inp" + i).addClass("animate flash correct");
+            amount += 1;
+            $("#basic-addon" + i).text(words[i - 1]);
+          }else{
+            $("#inp" + i).addClass("animate flash incorrect");
+            $("#basic-addon" + i).text(words[i - 1]);
+          }
+          $("#submit-btn").text(amount + "/5");
+          
+        }
+      });
     });
   });
 });
