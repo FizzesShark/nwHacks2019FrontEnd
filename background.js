889cf3caa251870b0632070387e1d52230432533
FizzesShark <xbug2001@gmail.com>
2019-01-26 19:58:48 -0800

    Added landing page, changed icon functionality

--- background.js
+++ background.js
@@ -1,3 +1,14 @@
 chrome.runtime.onInstalled.addListener(function() {
-  var userExists = false;
+  chrome.identity.getProfileUserInfo(function(e){
+    var userExists = $.get("https://mykatz.lib.id/repeatedli@dev/new_user?email=" + e.email);
+    if (!userExists){
+      window.open("newUser.html");
+    }
+  })
+});
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+  chrome.tabs.create({'url': chrome.extension.getURL('edit.html')}, function(tab) {
+    // Tab opened.
+  });
 });
--- cards.js
+++
@@ -1,4 +0,0 @@
-var user = chrome.identity.getProfileUserInfo(function(userInfo){
-    return userInfo;
-  });
-console.log(user.email);
