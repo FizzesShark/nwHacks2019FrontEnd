19fac96c5751a0e67f96567cd5521f36d69ad69d
FizzesShark <xbug2001@gmail.com>
2019-01-27 01:21:42 -0800

    Lots of updates

--- cards.js
+++ cards.js
@@ -1,7 +1,15 @@
 chrome.identity.getProfileUserInfo(function(e){
   $.get("https://mykatz.lib.id/repeatedli@dev/getWord?id=" + e.id, function(f){
-    var p = document.getElementById('flashCard');
-    p.innerHTML = f.word;
-    setTimeout(function(){p.innerHTML = f.translated}, 2000);
+    $("#flashCard").addClass("fadeIn");
+    $("#flashCard").text(f.word);
+    setTimeout(function(){
+      $("#flashCard").addClass("fadeOutUp");
+      $("#flashCard").text(f.translated);
+      setTimeout(function(){
+        $("#flashCard").removeClass("fadeOutUp");
+        $("#flashCard").addClass("fadeInUp");
+        console.log("fuck");
+      }, 500);
+    }, 3000);
   });
 });
